98ff36e0d98f9c285a840ea199170daabdf98055
oscarmrt <oscarmrt@hotmail.com>
2020-11-19 04:29:34 +0000

    modified file 0

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-
+//Star Wars
 request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, response, body) => {
   if (err) {
     console.log(err);
