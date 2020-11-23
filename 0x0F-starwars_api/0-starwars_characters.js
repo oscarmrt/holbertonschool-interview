e0cfc4e2b60b53246efcd8e7a66860b3c48f5ce6
oscarmrt <oscarmrt@hotmail.com>
2020-11-23 15:18:05 +0000

    fixed semistandard for file 0

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,25 +1,25 @@
 #!/usr/bin/node
 
-const request = require('request')
+const request = require('request');
 
-const apiurl = 'https://swapi.dev/api/films/' + process.argv.slice(2) + '/'
+const apiurl = 'https://swapi.dev/api/films/' + process.argv.slice(2) + '/';
 
 request(apiurl, async function (error, response, body) {
   if (error) {
-    console.log(error)
+    console.log(error);
   } else {
-    const characters = JSON.parse(body).characters
+    const characters = JSON.parse(body).characters;
     for (const url of characters) {
       const res = await new Promise((resolve, reject) => {
         request(url, (error, res, body2) => {
           if (error) {
-            reject(error)
+            reject(error);
           } else {
-            resolve(JSON.parse(body2).name)
+            resolve(JSON.parse(body2).name);
           }
-        })
-      })
-      console.log(res)
+        });
+      });
+      console.log(res);
     }
   }
-})
+});
