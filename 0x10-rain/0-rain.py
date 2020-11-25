1b7ab845d76b1e8c630cacaa1a451ebc3a3255ae
oscarmrt <oscarmrt@hotmail.com>
2020-11-25 21:04:09 +0000

    new directory, file 0 and README.md

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Program that given a list of non-negative integers
+representing walls of width 1, calculate how much water
+will be retained after it rains."""
+
+
+def rain(walls):
+    """Function that given a list of non-negative integers
+    representing walls of width 1, calculate how much water
+    will be retained after it rains."""
+    n = len(walls)
+    ans = 0
+    for i in range(1, n - 1): 
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1 , n): 
+            right = max(right, walls[j])
+        ans = ans + (min(left, right) - walls[i])
+    return ans
