c09a353e40bbcc241103d91262a4d68938eb8908
oscarmrt <oscarmrt@hotmail.com>
2020-11-25 21:08:41 +0000

    modified file 0 PEP8 and condition

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -8,14 +8,16 @@ def rain(walls):
     """Function that given a list of non-negative integers
     representing walls of width 1, calculate how much water
     will be retained after it rains."""
+    if(not len(walls)):
+        return 0
     n = len(walls)
     ans = 0
-    for i in range(1, n - 1): 
+    for i in range(1, n - 1):
         left = walls[i]
         for j in range(i):
             left = max(left, walls[j])
         right = walls[i]
-        for j in range(i + 1 , n): 
+        for j in range(i + 1, n):
             right = max(right, walls[j])
         ans = ans + (min(left, right) - walls[i])
     return ans
