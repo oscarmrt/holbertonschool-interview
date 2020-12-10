b7d6b5057b49b7b48eb59d9c0af276d4a1a903b0
oscarmrt <oscarmrt@hotmail.com>
2020-12-10 04:31:33 +0000

    fixed betty

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -1,13 +1,15 @@
 #include "search_algos.h"
 
 /**
- * advanced_binary - Function that searches for a value in a sorted array of integers
+ * advanced_binary - Function that searches for a value in a
+ * sorted array of integers
  *
  * @array: pointer to the first element of the array to search in
  * @size: number of elements in array
  * @value: value to search for
  *
- * Return:  If value is not present in array or if array is NULL, your function must return -1
+ * Return:  If value is not present in array or if array is
+ * NULL, your function must return -1
  */
 int advanced_binary(int *array, size_t size, int value)
 {
