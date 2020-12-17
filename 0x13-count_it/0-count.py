3d8662ab158c5c41c827950c4751cf5682fb45c7
oscarmrt <oscarmrt@hotmail.com>
2020-12-17 00:27:31 +0000

    modified file 0

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -5,7 +5,7 @@ containing the titles of all hot articles for a given subreddit.'''
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=""):
+def count_words(subreddit, wlist, after="", counting={}):
         """Recursive function that queries the Reddit API and returns a list
         containing the titles of all hot articles for a given subreddit."""
         if after is None:
@@ -20,8 +20,24 @@ def recurse(subreddit, hot_list=[], after=""):
         r = requests.get(url, headers=headers, allow_redirects=False)
         if str(r) != '<Response [200]>':
             return None
-        rjson = r.json()
-        hotPosts = rjson.get('data').get('children')
-        for post in hotPosts:
-            hot_list.append(post.get('data').get('title'))
-        return hot_list + recurse(subreddit, [], rjson.get('data').get('after'))
+        data = r.json().get('data')
+        after = data.get('after')
+        children_list = data.get('children')
+        for child in children_list:
+            title = child.get('data').get('title')
+            for word in wlist:
+                ocurrences = title.lower().split().count(word.lower())
+                if ocurrences > 0:
+                    if word in counting:
+                        counting[word] += ocurrences
+                    else:
+                        counting[word] = ocurrences
+        if after is not None:
+            return count_words(subreddit, wlist, after, counting)
+        else:
+            if not len(counting) > 0:
+                return
+            iterator = sorted(counting.items(),
+                              key=lambda kv: (-kv[1], kv[0]))
+            for key, value in iterator:
+                print('{}: {}'.format(key.lower(), value))
